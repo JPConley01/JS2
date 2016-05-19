5c8c861261ec16731b933c5f1b31011d19801328
John Conley <jpconley01@gmail.com>
2016-05-19 18:20:48 +0000

    completed js2 exercise

---
+++ js2.js
@@ -0,0 +1,6 @@
+
+var word = prompt("Enter anything");
+
+
+alert("You entered" + ":" + " " + word + "\n" + "There are" + " " + word.length + " " + "characters in the word" + "." + "\n" + word.charAt(2) + "\n" + word.toLowerCase() + "\n" + word.toUpperCase() + "\n" + word + " "+ "are good for you" + "." + "\n" + word.substring(2,5));
+ 
